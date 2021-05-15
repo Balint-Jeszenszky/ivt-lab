f7940c8ac05a8703f58b209de3a3069d8194f1ec
Balint Jeszenszky <kisjeszeni1234@gmail.com>
2021-05-15 15:18:04 +0200

    more tests for 100% coverage

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -169,4 +169,50 @@ public class GT4500Test {
     verify(mockSecondaryTorpedoStore, times(1)).isEmpty();
   }
 
+  @Test
+  public void primaryStoreFireAgainSeconIsEmptyInSingleMode() {
+    // Arrange
+    when(mockPrimaryTorpedoStore.fire(1)).thenReturn(true);
+    when(mockPrimaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(mockSecondaryTorpedoStore.fire(1)).thenReturn(false);
+    when(mockSecondaryTorpedoStore.isEmpty()).thenReturn(true);
+
+    // Act
+    boolean firstResult = ship.fireTorpedo(FiringMode.SINGLE);
+    boolean secondResult = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, firstResult);
+    assertEquals(true, secondResult);
+    verify(mockPrimaryTorpedoStore, times(2)).fire(1);
+    verify(mockPrimaryTorpedoStore, times(2)).isEmpty();
+    verify(mockSecondaryTorpedoStore, times(0)).fire(1);
+    verify(mockSecondaryTorpedoStore, times(1)).isEmpty();
+  }
+
+  @Test
+  public void primaryStoreNotFireAgainAllStoresEmptyInSingleMode() {
+    // Arrange
+    when(mockPrimaryTorpedoStore.fire(1)).thenReturn(true);
+    when(mockPrimaryTorpedoStore.isEmpty()).thenReturn(false);
+    when(mockSecondaryTorpedoStore.fire(1)).thenReturn(false);
+    when(mockSecondaryTorpedoStore.isEmpty()).thenReturn(true);
+
+    // Act
+    boolean firstResult = ship.fireTorpedo(FiringMode.SINGLE);
+
+    when(mockPrimaryTorpedoStore.fire(1)).thenReturn(false);
+    when(mockPrimaryTorpedoStore.isEmpty()).thenReturn(true);
+
+    boolean secondResult = ship.fireTorpedo(FiringMode.SINGLE);
+
+    // Assert
+    assertEquals(true, firstResult);
+    assertEquals(false, secondResult);
+    verify(mockPrimaryTorpedoStore, times(1)).fire(1);
+    verify(mockPrimaryTorpedoStore, times(2)).isEmpty();
+    verify(mockSecondaryTorpedoStore, times(0)).fire(1);
+    verify(mockSecondaryTorpedoStore, times(1)).isEmpty();
+  }
+
 }
